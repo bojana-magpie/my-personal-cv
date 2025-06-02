c210d46ef18ce6b127e94bd356a8b06b593d5af9
bojana-strachkovski <bojana.strachkovski@gmail.com>
2025-06-02 20:18:16 +0200

    Ready for deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-const container = document.getElementById("app");
-const root = createRoot(container);
-import "./scss/layout/_layout.scss";
+import "./scss/layout/_layout.scss"
 
 import Home from "./js/Pages/Home";
+const container = document.getElementById("app");
+const root = createRoot(container);
+
 const App = () => { return <Home/> }
 
 root.render(<App/>);
